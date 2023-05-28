e9af0d5bd2ae5139d714c664eb2a2563513a50af
Vishal Rathore <vishal.rathore@multitech.com>
2023-05-28 12:55:29 -0500

    finished the total car cost componet using useMemo hook

--- src/components/CarValue.tsx
+++ src/components/CarValue.tsx
@@ -1,7 +1,22 @@
-import { ReactElement } from 'react';
+import { ReactElement, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
 
 const CarValue = (): ReactElement => {
-  return <div>CarValue</div>;
+  const data = useSelector((state: RootState) => state.cars.data);
+  const searchTerm = useSelector((state: RootState) => state.cars.searchTerm);
+
+  const filteredCars = useMemo(() => {
+    return data.filter(car => {
+      return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+  }, [data, searchTerm]);
+
+  const totalCost = filteredCars.reduce((total, currentCar) => {
+    return total + currentCar.cost;
+  }, 0);
+
+  return <div className="car-value">Total Cost: ${totalCost}</div>;
 };
 
 export default CarValue;
